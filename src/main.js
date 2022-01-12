83829b61111e20872368161cf909909d44d07200
jylee1017 <jylee950215@gmail.com>
2022-01-12 23:44:55 +0900

    feat : fontawesome 아이콘 추가

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import locale from 'element-ui/lib/locale/lang/ko'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
+import '@/plugins/fontAwesomeIcon.js'
 
 Vue.config.productionTip = false
 
